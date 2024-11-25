40c9f3794df44441d1905a32e00a6a7f02915d69
Juanma <juanmavegacarrillo@gmail.com>
2024-11-25 11:02:06 +0100

    Feature: Completar lógica del menú

--- menu.py
+++ menu.py
@@ -6,6 +6,22 @@ def mostrar_menu():
     print("4- Dividir")
     print("5- Salir")
 
-    opcion = input("Seleccione una opción (1-5): ")
-    numero = input("Introduzca un número: ")
-    return opcion, numero
+    opcion = int(input("Elige una opción: "))
+    if opcion == 1:
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        print(sumar(a, b))
+    elif opcion == 2:
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        print(restar(a, b))
+    elif opcion == 3:
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        print(multiplicar(a, b))
+    elif opcion == 4:
+        a = float(input("Introduce el primer número: "))
+        b = float(input("Introduce el segundo número: "))
+        print(dividir(a, b))
+    elif opcion == 5:
+        print("Saliendo del programa.")
