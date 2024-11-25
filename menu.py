a7afc533b6913811233f9485ac2dce9ffc946636
Diego Robles <diegorobles031204@gmail.com>
2024-11-25 10:38:30 +0100

    MENU

---
+++ menu.py
@@ -0,0 +1,11 @@
+def mostrar_menu():
+    print("Menú de opciones")
+    print("1- Sumar")
+    print("2- Restar")
+    print("3- Multiplicar")
+    print("4- Dividir")
+    print("5- Salir")
+
+    opcion = input("Seleccione una opción (1-5): ")
+    numero = input("Introduzca un número: ")
+    return opcion, numero
